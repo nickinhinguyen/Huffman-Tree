554ec9248a5009650086bbc301ab56472e65b5cf
Lan Huynh <lanhuynh@Lans-MBP.lan>
2020-02-02 18:38:21 -0500

    first commit

---
+++ HuffmanDef.hs
@@ -0,0 +1,8 @@
+module HuffmanDef where
+
+data HuffmanTree = Leaf Int Char | Branch Int HuffmanTree HuffmanTree
+    deriving (Eq, Show)
+
+getFreq :: HuffmanTree -> Int
+getFreq (Leaf f _) = f
+getFreq (Branch f _ _) = f
